49276358970dfa1bf2358432528216bda25b9f64
Kruxinger <52739484+Kruxinger@users.noreply.github.com>
2025-10-20 10:42:46 +0200

    added gpas mock service and etl caller as python

---
+++ utils/input_trigger.py
@@ -0,0 +1,26 @@
+from http.client import responses
+
+import requests
+from requests import Session
+import json
+
+url = 'http://localhost:8080/mtb'
+
+def get_session():
+    session = Session()
+    session.verify = False
+    return session
+
+
+with open("valid_input.json", "r") as f:
+    payload = json.load(f)
+
+session = get_session()
+
+response = session.get(url)
+print(response.status_code)
+print(response.text)
+
+response = session.post(url, json=payload)
+print(response.status_code)
+print(response.text[:500])
---
+++ utils/mock_service_gpas.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, Response
+import random
+import string
+
+app = Flask(__name__)
+
+def random_string(length=20):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+@app.route("/gpas/gpasService", methods=["POST"])
+def gpas_service():
+    xml = request.data.decode("utf-8")
+
+    if "getOrCreatePseudonymFor" in xml:
+        value = random_string()
+        response_xml = f"""<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+                  xmlns:psn="http://psn.ttp.ganimed.icmvc.emau.org/">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <psn>{value}</psn>
+   </soapenv:Body>
+</soapenv:Envelope>"""
+        return Response(response_xml, mimetype='text/xml')
+
+    elif "createPseudonymFor" in xml:
+        value = random_string()
+        response_xml = f"""<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+                  xmlns:psn="http://psn.ttp.ganimed.icmvc.emau.org/">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <psn>{value}</psn>
+   </soapenv:Body>
+</soapenv:Envelope>"""
+        return Response(response_xml, mimetype='text/xml')
+
+    else:
+        return Response("Invalid request", status=400)
+
+if __name__ == "__main__":
+    app.run(port=8082)
