dcfef981d82a6d6e4e0aeb0970df0397d320cb06
sheinick <stephan.heinicke@med.uni-muenchen.de>
2025-10-30 08:41:16 +0100

    updated jackson to

--- src/main/kotlin/dev/dnpm/etl/processor/config/JacksonConfig.kt
+++ src/main/kotlin/dev/dnpm/etl/processor/config/JacksonConfig.kt
@@ -1,11 +1,12 @@
 package dev.dnpm.etl.processor.config
 
 import ca.uhn.fhir.context.FhirContext
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @Configuration
 class JacksonConfig {
@@ -14,14 +15,20 @@ class JacksonConfig {
         var fhirContext: FhirContext = FhirContext.forR4()
 
         @JvmStatic
-        fun fhirContext(): FhirContext {
-            return fhirContext
-        }
+        fun fhirContext(): FhirContext = fhirContext
     }
 
     @Bean
-    fun objectMapper(): ObjectMapper = ObjectMapper().registerModule(FhirResourceModule())
-        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS).registerModule(
-            JavaTimeModule()
-        )
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper()
+            .registerModule(FhirResourceModule())
+            .registerModule(JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            // ⚡ Tolerant konfigurieren:
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
+            .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true)
+            .configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true)
+            .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
+    }
 }
